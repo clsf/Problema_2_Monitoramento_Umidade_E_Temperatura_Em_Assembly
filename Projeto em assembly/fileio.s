e21ae5c5dfbb48fd22f824a68ea8c67285492392
Nicassio <88558329+nicassiosantos@users.noreply.github.com>
2023-12-25 19:48:39 -0300

    Comentários fileio.s

--- Projeto em assembly/fileio.s
+++ Projeto em assembly/fileio.s
@@ -6,42 +6,42 @@
 
 .include "unistd.s"
 
-.equ O_RDONLY, 0
-.equ O_WRONLY, 1
+.equ O_RDONLY, 0 @valor da flag para abrir o arquivo somente como leitura
+.equ O_WRONLY, 1 @valor da flag para abrir o arquivo somente como leitura
 .equ O_CREAT, 0100
-.equ S_RDWR, 0666
+.equ S_RDWR, 0666 @valor da flag para abrir o arquivo como leitura e escrita
 
-.macro openFile fileName, flags
-	ldr r0, =\fileName
-	mov r1, #\flags
+.macro openFile fileName, flags macro para abrir o arquivo com o as opções de leitura e escrita
+	ldr r0, =\fileName @Nome do arquivo 
+	mov r1, #\flags @flags sobre como abrir ele 
 	mov r2, #S_RDWR @ RW access rights
-	mov r7, #sys_open
+	mov r7, #sys_open @chamada do SO responsável por abrir e criar o arquivo
 	svc 0
 .endm
 
-.macro readFile fd, buffer, length
+.macro readFile fd, buffer, length @macro para ler um arquivo  
 	mov r0, \fd @ file descriptor
-	ldr r1, =\buffer
-	mov r2, #\length
-	mov r7, #sys_read
+	ldr r1, =\buffer @buffer do arquivo
+	mov r2, #\length @tamanho do arquivo
+	mov r7, #sys_read @chamada do SO responsável por ler um arquivo 
 	svc 0
 .endm
 
-.macro writeFile fd, buffer, length
-	mov r0, \fd @ file descriptor
-	ldr r1, =\buffer
-	mov r2, \length
-	mov r7, #sys_write
+.macro writeFile fd, buffer, length @macro para escrever em um arquivo
+	mov r0, \fd @ file descriptor 
+	ldr r1, =\buffer @buffer do arquivo
+	mov r2, \length @tamanho do arquivo 
+	mov r7, #sys_write @chamda do SO responsavél por escrever em um arquivo
 	svc 0
 .endm
 
-.macro flushClose fd
+.macro flushClose fd @macro para fechar um arquivo q foi aberto
 	@fsync syscall
-	mov r0, \fd
-	mov r7, #sys_fsync
+	mov r0, \fd @ file descriptor 
+	mov r7, #sys_fsync @Chamada do SO para sincronizar o estado do arquivo e armazena-lo
 	svc 0
 	@close syscall
 	mov r0, \fd
-	mov r7, #sys_close
+	mov r7, #sys_close @Chamada do SO para fechar o descritor do arquivo
 	svc 0
 .endm
